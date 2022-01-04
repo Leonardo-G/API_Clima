23aad7805e935b7819f16397032736ff04dcc294
leonardog9823 <guanuco43@gmail.com>
2022-01-04 03:29:52 -0300

    variable de entorno api key

--- js/app.js
+++ js/app.js
@@ -45,8 +45,7 @@ function mostrarError(mensaje){
 }
 
 function consultarAPI(ciudad, pais){
-    
-    const appId = "23cdca7a83d2b1085e307e3eab355a31";
+    const appId = process.env.API_KEY;
 
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
     
